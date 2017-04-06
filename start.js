e1c9030b16076409116b095da0921639868ff711
sai3010 <saiprasanth2007@gmail.com>
2017-04-06 22:34:20 +0530

    done

---
+++ start.js
@@ -0,0 +1,9 @@
+jQuery.noConflict();
+$(document).ready(function(){
+    $('.startbut').click(function(){
+        $('#name').addClass('smoky');
+        $('#name').addClass('smoky-mirror');
+    });
+
+    
+});
